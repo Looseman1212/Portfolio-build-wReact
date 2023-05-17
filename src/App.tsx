0070cd8657c2ce440b985dc39f2ba3c0f056f0cd
max reid <maxwilliamreid@gmail.com>
2023-05-17 12:59:45 +1000

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Landing from "./Components/Landing";
+import Experiences from "./Components/Experiences";
+
+function App() {
+  return (
+    <>
+      <Landing />
+      <Experiences />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Experiences.tsx
@@ -0,0 +1,7 @@
+import "../scss/Experiences.scss";
+
+const Experiences = () => {
+  return <div className="white-wrapper"></div>;
+};
+
+export default Experiences;
