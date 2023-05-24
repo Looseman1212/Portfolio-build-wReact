f0b5ed9c788f517ca985460ff6a482d66d6c42c4
max reid <maxwilliamreid@gmail.com>
2023-05-24 12:40:49 +1000

    added react-helmet package so that I can manipulate the HTML head. Added a favicon as well of a smiley w a cowboy hat

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import Landing from "./Components/Landing";
 import Experiences from "./Components/Experiences";
 import Drawers from "./Components/Drawers";
+import Projects from "./Components/Projects";
 import Photography from "./Components/Photography";
 import Moreme from "./Components/Moreme";
 
 import "./scss/App.scss";
-import Projects from "./Components/Projects";
+import favicon from "./assets/favicon-32x32.png";
+
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <>
+      <Helmet>
+        <title>Max Reid</title>
+        <meta
+          name="Max Reid Portfolio"
+          content="Portfolio for web developer Max Reid"
+        />
+        <link rel="icon" type="image/png" href={favicon} sizes="16x16" />
+      </Helmet>
       <Landing />
       <Experiences id="experiences" />
       <Drawers />
